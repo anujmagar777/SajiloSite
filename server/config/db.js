ee2853e87626046d9b799e1e7602221be3437a02
BipinGosai <bipin.gosai321@gmail.com>
2026-07-17 23:35:56 +0545

    Chatbox and prompt update

--- server/config/db.js
+++ server/config/db.js
@@ -1,15 +1,15 @@
 import mongoose from "mongoose"
 
-const connectDb = async()=>{
-    try{
-        if (!process.env.MONGODB_URL) {
-            throw new Error("MONGODB_URL is missing in environment variables")
-        }
+const connectDb = async () => {
+    if (!process.env.MONGODB_URL) {
+        throw new Error('MONGODB_URL is missing in environment variables')
+    }
+    try {
         await mongoose.connect(process.env.MONGODB_URL)
-        console.log("db connected")
-    } catch(error) {
-            console.error("db error", error.message)
-            throw error
+        console.log('db connected')
+    } catch (error) {
+        console.error('db error', error.message)
+        throw error
     }
 }
 
